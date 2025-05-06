8fe71ac89e155eeffb04026202f957395943d1e5
xxmoonscorionxx <22091wk@hvhs.school.nz>
2025-05-06 01:30:19 +0000

    broken

--- fb_io.mjs
+++ fb_io.mjs
@@ -1,7 +1,7 @@
 //**************************************************************/
 // fb_io.mjs
 // Generalised firebase routines
-// Written by <Your Name Here>, Term 2 202?
+// Written by <William Kan>, Term 2 202?
 //
 // All variables & function begin with fb_  all const with FB_
 // Diagnostic code lines have a comment appended to them //DIAG
@@ -9,25 +9,82 @@
 const COL_C = 'white';	    // These two const are part of the coloured 	
 const COL_B = '#CD7F32';	//  console.log for functions scheme
 console.log('%c fb_io.mjs',
-            'color: blue; background-color: white;');
+    'color: blue; background-color: white;');
 
 /**************************************************************/
 // Import all external constants & functions required
 /**************************************************************/
 // Import all the methods you want to call from the firebase modules
-
+import { initializeApp }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { getAuth, onAuthStateChanged }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
 /**************************************************************/
 // EXPORT FUNCTIONS
 // List all the functions called by code or html outside of this module
 /**************************************************************/
-export { 
-    fb_initialise };
+export {
+    fb_initialise, fb_authenticate, fb_onAuthStateChanged
+};
 
+
+/******************************************************/
+// fb_initialise()
+// Called by button initialise
+// Initialises the database connection
+// Input:  n/a
+// Return: n/a
+/******************************************************/
 function fb_initialise() {
-    console.log('%c fb_initialise(): ', 
-                'color: ' + COL_C + '; background-color: ' + COL_B + ';');
+    console.log('%c fb_initialise(): ',
+        'color: ' + COL_C + '; background-color: ' + COL_B + ';');
+    const firebaseConfig = {
+        apiKey: "AIzaSyAQ4FYhhhVQvTWxBJstBPqUEM7k1z3HNCs",
+        authDomain: "comp-2025-william-kan.firebaseapp.com",
+        databaseURL: "https://comp-2025-william-kan-default-rtdb.firebaseio.com",
+        projectId: "comp-2025-william-kan",
+        storageBucket: "comp-2025-william-kan.firebasestorage.app",
+        messagingSenderId: "928584832942",
+        appId: "1:928584832942:web:caa21627c817d307485a3f",
+        measurementId: "G-L6S6H3WPXE"
+    };
+    // Initialize Firebase
+    const FB_GAMEAPP = initializeApp(firebaseConfig);
+    const FB_GAMEDB = getDatabase(FB_GAMEAPP);
+    console.info(FB_GAMEDB);
+    document.getElementById("p_fbInitialise").innerHTML = "Initialised";
 }
+/******************************************************/
+// fb_authenticate()
+// Called by button initialise
+// Initialises the database connection
+// Input:  n/a
+// Return: n/a
+/******************************************************/
+function fb_authenticate() {
+    const AUTH = getAuth();
+    const PROVIDER = new GoogleAuthProvider();
+    // The following makes Google ask the user to select the account
+    PROVIDER.setCustomParameters({
+        prompt: 'select_account'
+    });
+    signInWithPopup(AUTH, PROVIDER).then((result) => {
+    })
+        .catch((error) => {
+        });
+    document.getElementById("p_fbAuthenticate").innerHTML = "Authenticated";
+}
+function fb_onAuthStateChanged() {
+    document.getElementById("p_fbLogin").innerHTML = "Logged in";
+}
+
+
+
 
 /**************************************************************/
 // END OF CODE
--- main.mjs
+++ main.mjs
@@ -5,17 +5,19 @@
 /**************************************************************/
 const COL_C = 'white';	    // These two const are part of the coloured 	
 const COL_B = '#CD7F32';	//  console.log for functions scheme
-console.log('%c main.mjs', 
+console.log('%c main.mjs',
     'color: blue; background-color: white;');
 
 /**************************************************************/
 // Import all external constants & functions required
 /**************************************************************/
 // Import all the constants & functions required from fb_io module
-import { fb_initialise }
+import { fb_initialise, fb_authenticate, fb_onAuthStateChanged }
     from './fb_io.mjs';
-    window.fb_initialise   = fb_initialise;
- 
+window.fb_initialise = fb_initialise;
+window.fb_authenticate = fb_authenticate;
+window.fb_authenticate = fb_onAuthStateChanged;
+
 /**************************************************************/
 // index.html main code
 /**************************************************************/
